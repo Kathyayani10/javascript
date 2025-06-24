c685a0d3176c5a0cc54e60c1845864364bdb122a
Kathyayani10 <Sangojukathyayani9999@gmail.com>
2025-06-24 15:42:27 +0530

    Initial commit

---
+++ intro.js
@@ -0,0 +1,36 @@
+let a=10;
+let b=5;
+ console.log("Arithmetic Operations");
+ console.log("Addition:",a+b);
+ console.log("Subtraction:",a-b);
+ console.log("mulitplication:",a*b);
+ console.log("division:",a/b);
+ console.log("modules:",a%b);
+ console.log("Exponentiation:",a**b);
+ let x=10;
+ let y=5;
+ console.log("\nAssignment Operators:");
+ x +=5;
+ y *=2;
+ console.log("x +=5:",x);
+ console.log("y *=2:",y)
+ console.log("\nComparsion Operation:");
+ console.log("Equal:",a==b);
+ console.log("Not Equal:",a!=b);
+ let q=2;
+ let r='2';
+ console.log("Equal with Type Coercian:",q==r);
+ console.log("Strict Equal:",a===b);
+ console.log("Strict  Not Equal:",a!==b);
+ let age=18;
+ let canVote=(age >=18) ? "you can vote" : "you cannot vote.";
+ console.log("\nTernary Operator:");
+ console.log("Result:",canVote);
+ console.log("\nTypeof Operator:");
+ 
+
+
+ 
+ 
+
+
